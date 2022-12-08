0d37cf2a79438d7b474889f0729baf97a076aa99
B-loeb <bradleyloeb@gmail.com>
2022-12-08 14:26:00 -0500

    Fixing error messages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
 ReactDOM.render(
   <BrowserRouter>
