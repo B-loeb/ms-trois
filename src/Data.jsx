150f5f5cc0f88f76a6ac29f3efbefbe687104f01
B-loeb <bradleyloeb@gmail.com>
2022-12-08 19:17:48 -0500

    fixed imgs in service cards

--- src/Data.jsx
+++ src/Data.jsx
@@ -4,14 +4,14 @@ const DATA= [
         title : "Rooftop Inspection",
         price : 100,
         desc : "Professional roof inspections help identify damages to the roof that are often invisible to the untrained eye. Traditional roof inspections can be risky, complex, and expensive for large complex structures. Newer technologies have come into play to make roof inspections safer, easier, faster, and relatively cheaper. Using a drone for inspecting rooftops is gaining popularity due to the amount of benefits it offers.",
-        img : "public/assets/images/products/img8.jpg"
+        img : "/assets/images/products/img8.jpg"
     },
     {
         id : 1,
         title : "Wedding Events",
         price : 400,
         desc : "Professional wedding photoshoot and/or capture large outdoor weddings live from interesting aerial views that will provide unique memories. This package can be tailored to fit individual needs",
-        img : "public/assets/images/products/img9.jpg"
+        img : "/assets/images/products/img9.jpg"
     }
 ]
 export default DATA
