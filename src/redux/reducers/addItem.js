14592c4587f6d34b7a777bfdafddecfc7d328ff7
B-loeb <bradleyloeb@gmail.com>
2022-12-10 17:59:27 -0500

    create addItem reducer/add redcers to rootReducers

--- src/redux/reducers/addItem.js
+++ src/redux/reducers/addItem.js
@@ -6,5 +6,19 @@ const addItems = (state = addItem, action) => {
             ...state,
             action.payload
         ]
+        break;
+
+        case "DELETEITEM" :
+            return state = state.filter((x)=>{
+                return x.id !== action.payload.id
+            })
+        break;
+
+        default: return state;
+        break;
+
+
     }
 }
+
+export default addItem;
